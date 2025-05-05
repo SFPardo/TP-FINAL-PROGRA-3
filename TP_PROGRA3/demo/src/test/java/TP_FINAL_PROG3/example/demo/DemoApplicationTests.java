719bd8687825d2f7c7ff6ce60cc0033d26a78672
Simon <simonfabianpardo@gmail.com>
2025-05-05 20:06:23 -0300

    BASE
    Generado desde spring.io

---
+++ TP_PROGRA3/demo/src/test/java/TP_FINAL_PROG3/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package TP_FINAL_PROG3.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
