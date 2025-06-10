c77755f85eccce316c78c58b3700c60170fa2318
TomasMacGuire <tomasmacguire@gmail.com>
2025-06-10 00:38:51 -0300

    10.6 - 0:38hs - Tomas
    - Initializer de Admin
    - Correccion metodos en Usuario y Cliente controller

--- TP_PROGRA3/Tp final/src/main/java/com/example/demo/services/impl/MovimientoTarjetaServiceImpl.java
+++ TP_PROGRA3/Tp final/src/main/java/com/example/demo/services/impl/MovimientoTarjetaServiceImpl.java
@@ -11,11 +11,12 @@ import com.example.demo.repositories.TarjetaDebitoRepository;
 import com.example.demo.services.MovimientoTarjetaService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MovimientoTarjetaServiceImpl implements MovimientoTarjetaService {
     @Autowired
     private MovimientoTarjetaRepository movimientoTarjetaRepository;
